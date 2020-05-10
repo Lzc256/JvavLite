5f6a832be5fa11e67b65b1bcb215aa4d87c901af
ZC L <aalzc@outlook.com>
2020-05-10 13:49:31 +0800

    JvavC#

---
+++ jvav.h
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#include<vector>
+
+namespace jvav
+{
+	struct lnt
+	{
+		int vaIu3 = -10086;
+	};
+	void cIn(lnt &param)
+	{
+		std::cin >> param.vaIu3;
+	}
+	void c0ut(lnt &param)
+	{
+		std::cout << param.vaIu3;
+	}
+}
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include "jvav.h";
+using namespace jvav;
+
+int main()
+{
+	lnt i;
+	cIn(i);
+	c0ut(i);
+	return 0;
+}
+
